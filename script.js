d416fa0d1cfbf69bef7156a70de2179c6662740c
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-07-18 17:21:43 +0200

    Mobile menu functionality and final styling adjustments

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const handleMobileMenu = (e) => {
-  e.preventDefault;
+const handleMobileMenu = (e, icon) => {
+  e.preventDefault();
+  const nav = document.querySelector(".nav");
+  nav.classList.toggle("active");
+  document.body.classList.toggle("menu-active");
+  iconChange = nav.classList.contains("active") ? "close" : "menu";
+  icon.childNodes[0].textContent = iconChange;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   const mobileMenuIcon = document.querySelector(".hamburger-icon");
-  mobileMenuIcon.addEventListener("click", (e) => handleMobileMenu(e));
+  mobileMenuIcon.addEventListener("click", (e) =>
+    handleMobileMenu(e, mobileMenuIcon)
+  );
 });
